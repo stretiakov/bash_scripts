c3a10b3a18a783b50095572b01827dddb477d278
Serhii Tretiakov <142805080+stretiakov@users.noreply.github.com>
2023-12-30 22:04:18 +0100

    Create weather.sh

---
+++ weather.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+# select place
+select place in casablanca amsterdam kiev
+do
+# silently downloading the weather report
+wrep=$(curl -s wttr.in/$place)
+break # without "break" it will be stuck in the select loop
+done
+
+# exctracting string with temperatures
+tps=$(echo $wrep | grep -oP '\S+\s+\xC2\xB0C' | cut -d ' ' -f1 | sed 's/\(([^)]*)\)//g')
+# the page code is searched for a regular expression '\S+\s+\xC2\xB0C', where xC2\xB0C is a centrigrade symbol 
+# -o outputs the matching part of the line and -P (Perl-compatible) enables allows for enhanced regexps
+# then the space-separated centrigrade symbol is cut off
+# and a grouping construct \(([^)]*)\) is applied to eliminiate (substitute for nothing, globally ) the parentheses 
+# and all therein, \( and \) are literally opening and closing parentheses, 
+# [^)] = ALL BUT a closing parantheses, * - zero or unlimited times
+# see more @ https://regex101.com/
+
+#converting the string to array
+IFS=$'\n' read -r -d '' -a temps <<< "$tps"
+
+echo "Current temperature is ${temps[0]} degrees"
+echo "Tomorrow at noon will be ${temps[2]} degrees"
